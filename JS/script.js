433e5707d220edbb5decd08334e69d021c49f5fc
Merabi-Michael <merabizamtaradze28@gmail.com>
2021-10-01 01:14:08 +0400

    Added Js

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+// function  mySlick(){
+//   $(".wrapper").not('.slick-initiaized').slick({
+//     centerMode: true,
+//     autoplay: true,
+//     dots: true,
+//     slideToShow: 3,
+//     responsive: [{
+//       breakpoint: 768,
+//       settings: {
+//         dots: false,
+//         arrows: false,
+//         Infinite:false,
+//         slideToShow: 1,
+//         slideToScroll: 1
+//       }
+//     }]
+//   });
+// }
+// mySlick();
+
+$('.wrapper').slick();
