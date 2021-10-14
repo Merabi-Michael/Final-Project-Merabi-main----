d2747cda31e7e5c954f2df1db30af6e8875c7d8c
Merabi-Michael <merabizamtaradze28@gmail.com>
2021-10-14 17:32:10 +0400

    I added a lot of items and almost done

--- JS/script.js
+++ JS/script.js
@@ -19,3 +19,11 @@
 // mySlick();
 
 $('.wrapper').slick();
+
+
+
+// burger
+let navBarLinks = document.getElementById('navbarLinks');
+let toggleButton = document.getElementById('toggleButton');
+toggleButton.addEventListener("click", function()
+navBarLinks.classList.toggle('active'); })
